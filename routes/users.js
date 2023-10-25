1f36833fd915250110e292e94aa9c4e6fddb3746
Rasool Shaik <rasoolshaik957307@gmail.com>
2023-10-25 11:55:49 -0500

    modified routes

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  more += 1;
-  sum += more;
+  more+=1;
+  sum+=more;
   res.send(`Sum is : ${sum}`);
 });
 
